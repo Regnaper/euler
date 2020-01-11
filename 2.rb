3c9daccb8649036d78ff67c6ebfd1e3a64506536
Regnaper <iv_vod@mail.ru>
2020-01-11 13:24:42 +0300

    Create 2.rb

---
+++ 2.rb
@@ -0,0 +1,17 @@
+def main(limit)
+  sum = 0
+  first = 1
+  second = 2
+  flag = true
+  while second < limit
+    sum += second if flag
+    first, second = second, first + second
+    flag = flag ? false : true
+  end
+  sum
+end
+
+t1 = Time.new
+puts main(2000*2000)
+t1 = Time.new - t1
+puts t1
