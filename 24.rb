a49f380f790f963d0a1194ffca654ab7a74633a6
Regnaper <iv_vod@mail.ru>
2020-02-10 22:55:22 +0300

    Update 24.rb

--- 24.rb
+++ 24.rb
@@ -2,8 +2,8 @@ def narayana(arr, iter)
   """
     Выполняем вычисления одного из трех шагов Алгоритма Нарайаны.
   """
-  8.downto(0).each { |j| return j if arr[j] < arr[j+1] } if iter == 1
-  9.downto(0).each { |l| return l if arr[l] > arr[$j] } if iter == 2
+  (arr.size-2).downto(0).each { |j| return j if arr[j] < arr[j+1] } if iter == 1
+  (arr.size-1).downto(0).each { |l| return l if arr[l] > arr[$j] } if iter == 2
   if iter == 3
     arr[$j], arr[$l] = arr[$l], arr[$j]
     arr[$j+1..-1] = arr[$j+1..-1].reverse
@@ -11,12 +11,12 @@ def narayana(arr, iter)
   end
 end
 
-def main(limit)
+def main(number, limit)
   """
     Используя Алгоритм Нарайаны, в каждом шаге цикла получаем следующую перестановку.
   """
   count = 1
-  arr = '0123456789'.chars
+  arr = number.to_s.chars
   while count < limit do
     $j = narayana(arr, 1)
     $l = narayana(arr, 2)
@@ -27,6 +27,6 @@ def main(limit)
 end
 
 t1 = Time.new
-puts main 1000000
+puts main('0123456789', 1000000)
 t1 = Time.new - t1
 puts("Spent time: #{t1}")
