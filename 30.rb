20954d7e2b38a2431f419059293f817a890eab22
Regnaper <iv_vod@mail.ru>
2020-02-16 21:17:39 +0300

    Create 30.rb

---
+++ 30.rb
@@ -0,0 +1,12 @@
+def get_5_degree(number)
+  num = number.to_s.split(//).inject(0) { |sum, n| sum + n.to_i**5 }
+end
+
+def main(limit)
+  (2..limit).inject(0) { |sum, n| get_5_degree(n) == n ? sum + n : sum }
+end
+
+t1 = Time.new
+puts main(200000)
+t1 = Time.new - t1
+puts("Spent time: #{t1}")
