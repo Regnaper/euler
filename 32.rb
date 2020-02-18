991b0cda4c096a6b7dded79540113ab73e9593a9
Regnaper <iv_vod@mail.ru>
2020-02-18 22:46:46 +0300

    Create 32.rb

---
+++ 32.rb
@@ -0,0 +1,22 @@
+def pandigital(a, b, c)
+  arr = a | b | c
+  return true if arr.sort == (1..9).to_a && arr.size == (a + b + c).size
+  false
+end
+
+def main()
+  pandigitals = []
+  (1..99).each do |a|
+    limit = ('9' * (5 - a.digits.size)).to_i
+    (1..limit).each do |b|
+      c = a * b
+      pandigitals << c if pandigital(a.digits, b.digits, c.digits)
+    end
+  end
+  pandigitals.uniq.reduce(0, :+)
+end
+
+t1 = Time.new
+puts main
+t1 = Time.new - t1
+puts("Spent time: #{t1}")
