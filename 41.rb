2f2efe382d83919ef81b05a223ce797bac562a3a
Regnaper <iv_vod@mail.ru>
2020-03-01 17:52:00 +0300

    Update 41.rb

--- 41.rb
+++ 41.rb
@@ -1,14 +1,9 @@
 require 'prime'
 
-def pandigital?(digits)
-  """Функция принимает массив цифр числа"""
-  return true if digits.sort == (1..digits.size).to_a
-end
-
 def main()
   9.downto(2).each do |n|
     n.downto(1).to_a.permutation.to_a.each do |num| # перебираем все перестановки от n до 1
-      return num.join.to_i if Prime.prime?(num.join.to_i) && pandigital?(num)
+      return num.join.to_i if Prime.prime?(num.join.to_i)
     end
   end
 end
